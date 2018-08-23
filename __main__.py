d1d985b58f406e54b625460c381edc63e2b2e19a
erathiian <emaola0404@gmail.como>
2018-08-23 09:52:37 +0200

    test

--- __main__.py
+++ __main__.py
@@ -1,8 +1,19 @@
-
+import pygame
 
 
 def main():
-    pass
+    pygame.init()
+
+    pygame.display.set_caption('Expanded Brain')
+
+    screen = pygame.display.set_mode((300, 300))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
 
 if __name__ == '__main__':
     main()
