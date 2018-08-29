6f01aecd6db14bde2e7eb4488d5b749131510fe1
Benjamin Sannholm <benjamin@sannholm.se>
2018-08-29 10:42:37 +0200

    Add another function and only reload functions from file every 30 frames

--- __main__.py
+++ __main__.py
@@ -1,11 +1,10 @@
 import pygame
 import math
 import random
-import sys
 
 SCREEN_SIZE = (640, 480)
-MIN = (random.uniform(-1, -10), random.uniform(-1, -10))
-MAX = (random.uniform(1, 10), random.uniform(1, 10))
+MIN = (random.uniform(-5, -10), random.uniform(-5, -10))
+MAX = (random.uniform(5, 10), random.uniform(5, 10))
 
 FONT = None
 SCREEN = None
@@ -104,88 +103,88 @@ def main():
     functions = []
 
     running = True
-    done_drawing = False
+
+    frame_number = 0
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        try:
-            functions = load_functions_from_file(file_path)
-            done_drawing = False
-        except Exception as error:
-            print(error)
-        if not done_drawing:
-            SCREEN.fill((0,0,0))
-            axis_color = (70, 70, 70)
-            grid_color = (30, 30, 30)
-            marker_color = (70, 70, 70)
+        if frame_number % 30 == 0:
+            try:
+                functions = load_functions_from_file(file_path)
+            except Exception as error:
+                print(error)
 
-            window_x = MAX[0] - MIN[0]
-            x_grid_spacing = 1000000000
-            while window_x / x_grid_spacing <= 2:
-                x_grid_spacing /= 10
+        SCREEN.fill((0,0,0))
+        axis_color = (70, 70, 70)
+        grid_color = (30, 30, 30)
+        marker_color = (70, 70, 70)
 
-            num_lines = int(window_x / x_grid_spacing)
-            for x in range(1, num_lines + 1):
-                x_coord = MIN[0] + x * x_grid_spacing - MIN[0] % x_grid_spacing
-                pygame.draw.line(SCREEN, grid_color,
-                                coord_to_pxl((x_coord, MIN[1]), MIN, MAX),
-                                coord_to_pxl((x_coord, MAX[1]), MIN, MAX))
+        window_x = MAX[0] - MIN[0]
+        x_grid_spacing = 1000000000
+        while window_x / x_grid_spacing <= 2:
+            x_grid_spacing /= 10
 
-                marker_pixel_coord = coord_to_pxl((x_coord, 0), MIN, MAX)
-                pygame.draw.line(SCREEN, marker_color,
-                                marker_pixel_coord,
-                                (marker_pixel_coord[0], marker_pixel_coord[1] - 10))
+        num_lines = int(window_x / x_grid_spacing)
+        for x in range(1, num_lines + 1):
+            x_coord = MIN[0] + x * x_grid_spacing - MIN[0] % x_grid_spacing
+            pygame.draw.line(SCREEN, grid_color,
+                            coord_to_pxl((x_coord, MIN[1]), MIN, MAX),
+                            coord_to_pxl((x_coord, MAX[1]), MIN, MAX))
 
-                text_coord = (marker_pixel_coord[0], marker_pixel_coord[1] + 5)
-                if is_zero(x_coord, x_grid_spacing):
-                    text_coord = (marker_pixel_coord[0] + 10, marker_pixel_coord[1] + 5)
-                
-                render_text(format_number(x_coord, x_grid_spacing), text_coord, marker_color, (0.5, 0))
+            marker_pixel_coord = coord_to_pxl((x_coord, 0), MIN, MAX)
+            pygame.draw.line(SCREEN, marker_color,
+                            marker_pixel_coord,
+                            (marker_pixel_coord[0], marker_pixel_coord[1] - 10))
 
-            window_y = MAX[1] - MIN[1]
-            y_grid_spacing = 1000000000
-            while window_y / y_grid_spacing <= 3:
-                y_grid_spacing /= 10
+            text_coord = (marker_pixel_coord[0], marker_pixel_coord[1] + 5)
+            if is_zero(x_coord, x_grid_spacing):
+                text_coord = (marker_pixel_coord[0] + 10, marker_pixel_coord[1] + 5)
 
-            num_lines = int(window_y / y_grid_spacing)
-            for y in range(1, num_lines + 1):
-                y_coord = MIN[1] + y * y_grid_spacing - MIN[1] % y_grid_spacing
-                pygame.draw.line(SCREEN, grid_color,
-                                coord_to_pxl((MIN[0], y_coord), MIN, MAX),
-                                coord_to_pxl((MAX[0], y_coord), MIN, MAX))
+            render_text(format_number(x_coord, x_grid_spacing), text_coord, marker_color, (0.5, 0))
 
-                marker_pixel_coord = coord_to_pxl((0, y_coord), MIN, MAX)
-                pygame.draw.line(SCREEN, marker_color,
-                                marker_pixel_coord,
-                                (marker_pixel_coord[0] + 10, marker_pixel_coord[1]))
+        window_y = MAX[1] - MIN[1]
+        y_grid_spacing = 1000000000
+        while window_y / y_grid_spacing <= 3:
+            y_grid_spacing /= 10
 
-                if not is_zero(y_coord, y_grid_spacing):
-                    text_coord = (marker_pixel_coord[0] - 5, marker_pixel_coord[1])
-                    render_text(format_number(y_coord, y_grid_spacing), text_coord, marker_color, (1, 0.5))
+        num_lines = int(window_y / y_grid_spacing)
+        for y in range(1, num_lines + 1):
+            y_coord = MIN[1] + y * y_grid_spacing - MIN[1] % y_grid_spacing
+            pygame.draw.line(SCREEN, grid_color,
+                            coord_to_pxl((MIN[0], y_coord), MIN, MAX),
+                            coord_to_pxl((MAX[0], y_coord), MIN, MAX))
 
-            pygame.draw.line(SCREEN, axis_color,
-                            coord_to_pxl((0, MIN[1]), MIN, MAX),
-                            coord_to_pxl((0, MAX[1]), MIN, MAX))
-            pygame.draw.line(SCREEN, axis_color,
-                            coord_to_pxl((MIN[0], 0), MIN, MAX),
-                            coord_to_pxl((MAX[0], 0), MIN, MAX))
+            marker_pixel_coord = coord_to_pxl((0, y_coord), MIN, MAX)
+            pygame.draw.line(SCREEN, marker_color,
+                            marker_pixel_coord,
+                            (marker_pixel_coord[0] + 10, marker_pixel_coord[1]))
 
-            for function in functions:
-                line_coords = []
-                for idx in range(SCREEN_SIZE[0]):
-                    x = x_pxl_to_coord(idx, MIN[0], MAX[0])
-                    y = function({ "x": x, "time": pygame.time.get_ticks() / 1000.0 })
-                    line_coords.append(coord_to_pxl((x, y), MIN, MAX))
+            if not is_zero(y_coord, y_grid_spacing):
+                text_coord = (marker_pixel_coord[0] - 5, marker_pixel_coord[1])
+                render_text(format_number(y_coord, y_grid_spacing), text_coord, marker_color, (1, 0.5))
 
-                pygame.draw.lines(SCREEN, function.color, False, line_coords)
+        pygame.draw.line(SCREEN, axis_color,
+                        coord_to_pxl((0, MIN[1]), MIN, MAX),
+                        coord_to_pxl((0, MAX[1]), MIN, MAX))
+        pygame.draw.line(SCREEN, axis_color,
+                        coord_to_pxl((MIN[0], 0), MIN, MAX),
+                        coord_to_pxl((MAX[0], 0), MIN, MAX))
 
-            pygame.display.update()
+        for function in functions:
+            line_coords = []
+            for idx in range(SCREEN_SIZE[0]):
+                x = x_pxl_to_coord(idx, MIN[0], MAX[0])
+                y = function({ "x": x, "time": pygame.time.get_ticks() / 1000.0 })
+                line_coords.append(coord_to_pxl((x, y), MIN, MAX))
 
-            done_drawing = True
+            pygame.draw.lines(SCREEN, function.color, False, line_coords)
 
+        pygame.display.update()
+
+        frame_number += 1
 
 
 if __name__ == '__main__':
